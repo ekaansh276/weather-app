3608340d692af40aeae9dd749a1194097c4e1470
Ekaansh <ekaanshdas276@gmail.com>
2024-06-28 20:20:36 +0530

    First commit
    create-react-app to get modules

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "acd70d9325msh5fc129693ebe666p1a0a96jsnf1a39263bb2e",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "9e386c2b01066b4e5066aa564d9f9ecc";
